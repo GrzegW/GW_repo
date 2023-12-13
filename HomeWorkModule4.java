775b4ffe060385c6ae15d92cfc2ca62bb1722f7b
GrzegW <g.wocial@gmail.com>
2023-12-13 10:36:10 +0100

    1 commit

---
+++ HomeWorkModule4.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HomeWorkModule4 {
+    public static void main(String[] args) {
+        int year = getYear();
+
+        int Aga = 1987;
+        int Natalia = 2014;
+        int Ania = 2016;
+        int Grzegorz = 1986;
+
+        int ageAga = year - Aga;
+        int ageNatalia = year - Natalia;
+        int ageAnia = year - Ania;
+        int ageGrzegorz = year - Grzegorz;
+
+        displayAge(ageAga, ageNatalia, ageAnia, ageGrzegorz);
+
+        int sumOfAge = sum(ageAga, ageNatalia, ageAnia, ageGrzegorz);
+
+    }
+
+    static int sum(int a, int b, int c, int d) {
+        int sumOfAge = a + b + c + d;
+        System.out.println("***********************");
+        System.out.println("Wszyscy łącznie mamy " + sumOfAge + " lat!");
+        return sumOfAge;
+    }
+
+    static void displayAge(int ageAga, int ageNatalia, int ageAnia, int ageGrzegorz) {
+        System.out.println("Aga ma " + ageAga + " lat.");
+        System.out.println("Natalia ma " + ageNatalia + " lat.");
+        System.out.println("Ania ma " + ageAnia + " lat.");
+        System.out.println("Grzegorz ma " + ageGrzegorz + " lat.");
+    }
+
+    static int getYear() {
+        int year = LocalDate.now().getYear();
+        System.out.println("Obecny rok => " + year);
+        return year;
+    }
+
+@Test
+void sum() {
+    //given
+    int a = 37;
+    int b = 36;
+    int c = 9;
+    int d = 7;
+    //when
+    int sumOfAge = sum(a, b, c, d);
+    //then
+    assertEquals(89, sumOfAge);
+    }
+
+
+}
